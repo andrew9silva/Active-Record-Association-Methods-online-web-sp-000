e8ff60bb8a1077e909986cb4f3a19cf352faf1a0
Andrew Silva <andrewsilva9@gmail.com>
2019-10-09 01:32:08 +0000

    Automatically backed up by Learn

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -9,10 +9,7 @@ class Genre < ActiveRecord::Base
 
   def artist_count
     # return the number of artists associated with the genre
-    artist = self.songs.collect do |x|
-      x.artists
-    end
-    artist.count
+    self.artists.count
   end
 
   def all_artist_names
